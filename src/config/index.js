53a46406edf33da1a79fe160759da09e8458d8cb
Victor J. Rosario V <victorrosario.1890@gmail.com>
2022-06-27 08:59:00 -0400

    working ✅

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,3 @@
-import "dotenv/config";
-
 const config = {
   urlResume: `${process.env.REACT_APP_RESUME_URL}`,
   urlContact: `${process.env.REACT_APP_REQUEST_URL}`,
--- src/services/careerInfo.js
+++ src/services/careerInfo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { DiWindows } from "react-icons/di";
 import { BsTerminal } from "react-icons/bs";
 import { GoBrowser } from "react-icons/go";
@@ -7,27 +7,28 @@ const careerInfo = [
   {
     id: 1,
     icon: <DiWindows />,
-    title: `Operative System`,
-    description: `I consider myself a super curious person and when it comes to experimenting with OS and I consider all this to be a great experience.`,
-    knowledgeList: `Windows, GNU/Linux: Ubuntu, Docker (basic)`,
+    title: "Operative System",
+    description:
+      "I consider myself a super curious person and when it comes to experimenting with OS and I consider all this to be a great experience.",
+    knowledgeList: "Windows, GNU/Linux: Ubuntu, Docker (basic)"
   },
   {
     id: 2,
     icon: <GoBrowser />,
     title: "Frontend Development",
     description:
-      "I consider the visual part to be important to us and I think. I am the person to create great experiences for your websites.",
-    knowledgeList:
-      "HTML5, CSS3, JavaScript, Sass, React.JS, Next.JS, WordPress",
+      'I consider the visual part to be important to us and I think. I am the person to create great experiences for your websites.',
+    knowledgeList: "HTML5, CSS3, JavaScript, Sass, React.JS, Next.JS, WordPress"
   },
   {
     id: 3,
     icon: <BsTerminal />,
     title: "Backend Develoment & Others",
-    description: `A visually impressive site but without functionality, is the same as being rich and not knowing what to spend on, to avoid this, I am also a person capable of creating both visual and functional websites.`,
+    description:
+      "A visually impressive site but without functionality, is the same as being rich and not knowing what to spend on, to avoid this, I am also a person capable of creating both visual and functional websites.",
     knowledgeList:
-      "Git, JavaScript, TypeScript, Node JS, SQL/PL, PostgreSQL, TypeORM, Sequelize, etc",
-  },
+      'Git, JavaScript, TypeScript, Node JS, SQL/PL, PostgreSQL, TypeORM, Sequelize, etc'
+  }
 ];
 
 export default careerInfo;
